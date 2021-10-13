fac562a2f641626bcb06c362b20c09c4405a4200
Sibusiso Nkambule <snkambule@gmail.com>
2021-10-13 12:05:56 +0100

    Initial Commit

--- form.js
+++ form.js
@@ -1,59 +1,85 @@
-
 //validation
-function cFunction(){
 
-  var name = document.getElementById("first-name").value;
-  var lastName = document.getElementById("surname").value;
-  var phone = document.getElementById("telephone").value;
-  var checkEmail = document.getElementById("mail").value;
-  var message = document.getElementById("message").value;
-  var error_message = document.getElementById("error_message");
-  error_message.style.padding = "10px";
+  function cFunction() {
+	var firstName = document.forms["form1"]["first-name"];
+	var lastName = document.forms["form1"]["surname"];
+	var checkEmail = document.forms["form1"]["mail"];
+    var phone = document.getElementById("telephone");
+	var message = document.forms["form1"]["message"];
+
+	if (firstName.value == "" || firstName.length < 2) {
+		window.alert("Please enter your first name.");
+		firstName.focus();
+		return false;
+	}
+
+	if (lastName.value == ""|| lastName.length < 2) {
+		window.alert("Please enter your last name.");
+		lastName.focus();
+		return false;
+	}
+    
+	/*function testEmailAddress(checkEmail) {
+		// check for @
+		var at = checkEmail.indexOf("@");
+		if(at < 1){
+			window.alert("Invalid email")
+			return false} ;
+	
+		var fullStop = checkEmail.indexOf(".");
+		if(fullStop <= at + 2) {
+			window.alert("Invalid email")
+		    return false
+		};
+	
+		// check that the dot is not at the end
+		if (fullStop === checkEmail.length - 1){
+			window.alert("Invalid email")
+			return false
+		};
+	
+		return true;
+	}
+*/
+
+
+	if (checkEmail.value == "" || checkEmail.value.indexOf("@") == -1) {
+		window.alert(
+		  "Please enter a valid e-mail address.");
+		checkEmail.focus();
+		return false;
+	}
+	
+	if(phone == ""){
+		window.alert(
+			"Please enter a valid phone number.");
+		  return false;
+	  }
+
+	if (message.value == ""|| message.value.length <= 20) {
+		window.alert("Please enter a message with 20 characters or more than 20 characters");
+		message.focus();
+		return false;
+	}
+	
+   //modal open
+
+  document.querySelector('#finish').addEventListener("click", function() {
+	document.querySelector('.container-modal').style.display = "block";
+  });
+ 
+  //modal close
+  document.querySelector('.container-modal').addEventListener("click", function() {
+	document.querySelector('.container-modal').style.display = "none";
+  });
   
-  var errorText;
+  //display on console
+	console.log(firstName.value);
+	console.log(lastName.value)
+	console.log(checkEmail.value);
+	console.log(phone.value);
+	console.log(message.value);
 
-  if(name.length < 2){
-    errorText = "Please Enter valid Name";
-    error_message.innerHTML = errorText;
-    return false;
-  }
-
-  if(lastName.length < 2){
-    errorText = "Please Enter valid Name";
-    error_message.innerHTML = errorText;
-    return false;
-  }
-  if(isNaN(phone) || phone.length != 10){
-    errorText = "Please Enter valid Phone Number";
-    error_message.innerHTML = errorText;
-    return false;
-  }
-  if(checkEmail.indexOf("@") == -1){
-    errorText = "Please Enter valid Email";
-    error_message.innerHTML = errorText;
-    return false;
-  }
-  if(message.length <= 20){
-    errorText = "Please Enter More Than 20 Characters";
-    error_message.innerHTML = errorText;
-    return false;
-  }
-  //modal open
-document.querySelector('#finish').addEventListener("click", function() {
-  document.querySelector('.container-modal').style.display = "block";
-});
-//modal close
-document.querySelector('.container-modal').addEventListener("click", function() {
-  document.querySelector('.container-modal').style.display = "none";
-});
-//modal close outside
-
-
-//display on console
-  console.log(name);
-  console.log(lastName)
-  console.log(checkEmail);
-  console.log(phone);
-  console.log(message);
-  return true;
+	return true;
 }
+  
